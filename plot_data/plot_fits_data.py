727405f1de77f0f2f885f74db6c19a801e5b7681
Lukas <lukas.diehl@web.de>
2024-12-13 15:42:21 +0100

    refactoring

--- plot_data/plot_fits_data.py
+++ plot_data/plot_fits_data.py
@@ -1,6 +1,54 @@
 import matplotlib.pyplot as plt
 
 
+
+
+def plot_avg_rms(fits_data):
+    avg_dict = None
+    rms_dict = None
+
+    galaxy_name = list(fits_data.keys())[0].split("_")[0]
+
+    for key, item in fits_data.items():
+
+        if "avg".casefold() in key.casefold():
+            avg_dict = item
+        elif "rms".casefold() in key.casefold():
+            rms_dict = item
+
+    if avg_dict is None:
+        raise Exception("Missing AVG data")
+
+    if rms_dict is None:
+        raise Exception("Missing RMS data")
+
+    wavelengths = avg_dict["x_axis"][0]
+
+    avg_flux = avg_dict["data"][0]
+    rms_flux = rms_dict["data"][0]
+
+    avg_label = (r"$F_\lambda \, (\mathrm{AVG}) \, [\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1} \, "
+                 r"\mathrm{\AA}^{-1}]$")
+
+    rms_label = (r"$F_\lambda \, (\mathrm{RMS}) \, [\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1} \, "
+                 r"\mathrm{\AA}^{-1}]$")
+
+    wavelengths_label = r"Rest Wavelength $[\mathrm{\AA}]$"
+
+    avg_titel = "AVG"
+    rms_title = "RMS"
+
+    plot_two_spectra(wavelengths,
+                     avg_flux,
+                     rms_flux,
+                     wavelengths_label,
+                     avg_label,
+                     rms_label,
+                     avg_titel,
+                     rms_title,
+                     galaxy_name)
+
+
 def plot_two_spectra(x, y1, y2, xlabel, ylabel1, ylabel2, title1, title2, super_title):
     """
     Plots two spectra with the same x-axis in a single figure with two stacked subplots.
@@ -51,49 +99,3 @@ def plot_two_spectra(x, y1, y2, xlabel, ylabel1, ylabel2, title1, title2, super_
 
     # Plot anzeigen
     plt.show()
-
-
-def plot_avg_rms(fits_data):
-    avg_dict = None
-    rms_dict = None
-
-    galaxy_name = list(fits_data.keys())[0].split("_")[0]
-
-    for key, item in fits_data.items():
-
-        if "avg".casefold() in key.casefold():
-            avg_dict = item
-        elif "rms".casefold() in key.casefold():
-            rms_dict = item
-
-    if avg_dict is None:
-        raise Exception("Missing AVG data")
-
-    if rms_dict is None:
-        raise Exception("Missing RMS data")
-
-    wavelengths = avg_dict["x_axis"][0]
-
-    avg_flux = avg_dict["data"][0]
-    rms_flux = rms_dict["data"][0]
-
-    avg_label = (r"$F_\lambda \, (\mathrm{AVG}) \, [\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1} \, "
-                 r"\mathrm{\AA}^{-1}]$")
-
-    rms_label = (r"$F_\lambda \, (\mathrm{RMS}) \, [\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1} \, "
-                 r"\mathrm{\AA}^{-1}]$")
-
-    wavelengths_label = r"Rest Wavelength $[\mathrm{\AA}]$"
-
-    avg_titel = "AVG"
-    rms_title = "RMS"
-
-    plot_two_spectra(wavelengths,
-                     avg_flux,
-                     rms_flux,
-                     wavelengths_label,
-                     avg_label,
-                     rms_label,
-                     avg_titel,
-                     rms_title,
-                     galaxy_name)
