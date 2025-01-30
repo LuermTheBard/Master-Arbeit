038266a0c7773e0e9a1dab983be1b603e05186dd
Lukas <lukas.diehl@web.de>
2025-01-30 13:02:35 +0100

    Refactored group plot, Part 5 introduce new file

---
+++ plot_data/plot_1D_lightcurves_in_groups_data.py
@@ -0,0 +1,183 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator, MaxNLocator, FuncFormatter
+
+from plot_data.general_plot import prepare_data, finalize_figure, format_relative_days, format_yaxis, format_month_day
+from settings import BASE_MJD, COLORCODE_CONTINUA_NORMALIZED
+
+
+def plot_all_1d_lightcurves_in_groups(galaxie_campaigns_dict, output_dir, compare_cont, key_order=None, save_only=False):
+    base_mjd = BASE_MJD
+
+    xlabel = f"MJD - {base_mjd:.2f}"
+    ylabel_cont = (r"$F_{\lambda}$ $[\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1} \, "
+             r"\mathrm{\AA}^{-1}]$")
+    ylabel_line = (r"$F_{\lambda}$ $[\mathrm{erg} \, \mathrm{cm}^{-2} \, \mathrm{s}^{-1}]$")
+    yerr_name = 'fluxerrs [ergs/s/cm2/A]'
+
+
+
+    x_key = 'timestamps [MJD]'
+    y_key = 'fluxes [ergs/s/cm2/A]'
+
+    save_folder = output_dir / "plot_1d_lightcurves"
+    save_folder.mkdir(parents=True, exist_ok=True)
+
+    for campaign, data_dict in galaxie_campaigns_dict.items():
+        # Plot for lines
+        super_title = f"{campaign} Lines"
+        compare_cont_data = {compare_cont: data_dict["continua"][compare_cont]}
+        compare_cont_data.update(data_dict["lines"])
+
+        def sort_keys(key):
+            for idx, prefix in enumerate(key_order):
+                if key.startswith(prefix):
+                    return idx
+            return len(key_order)
+
+        sorted_line_data_dict = dict(sorted(compare_cont_data.items(), key=lambda item: sort_keys(item[0])))
+
+        plot_lightcurves_in_groups(sorted_line_data_dict, x_key, y_key, compare_cont, xlabel, ylabel_line, yerr_name=yerr_name, title=super_title,
+                               save_only=save_only, output_dir=save_folder)
+
+        # Plot for continua (with custom color dictionary if needed)
+        super_title = f"{campaign} Continua"
+        plot_lightcurves_in_groups(data_dict["continua"], x_key, y_key, compare_cont, xlabel, ylabel_cont, yerr_name=yerr_name,
+                               title=super_title, save_only=save_only, output_dir=save_folder,
+                               color_dict=COLORCODE_CONTINUA_NORMALIZED)
+
+
+def plot_lightcurves_in_groups(data, x_key, y_key, compare_cont, xlabel='X-axis', ylabel='Y-axis', shared_y=False,
+                               yerr_name=None, title=None, save_only=False,
+                               output_dir=None, color_dict=None, rows=4, cols=2):
+    """
+    Plots 1D-Daten in Gruppen für Lightcurves.
+
+    Parameter:
+    -----------
+    data : dict
+        Dictionary containing the data for the plots.
+    x_key : str
+        Key für die x-Daten in den Dictionaries der Daten.
+    y_key : str
+        Key für die y-Daten in den Dictionaries der Daten.
+    xlabel : str, optional
+        Label für die X-Achse (nicht immer verwendet).
+    ylabel : str, optional
+        Label für die Y-Achse.
+    shared_y : bool, optional
+        Wenn True, teilen sich alle Subplots die Y-Achse.
+    yerr_name : str, optional
+        Key für die Fehlerbalken im Dictionary.
+    title : str, optional
+        Titel für die gesamte Figure.
+    save_only : bool, optional
+        Ob die Abbildungen gespeichert werden sollen, ohne sie anzuzeigen.
+    output_dir : str or Path, optional
+        Verzeichnis, in dem die Abbildungen gespeichert werden.
+    color_dict : dict, optional
+        Dictionary mit Farben für jede Datenserie.
+    rows : int, optional
+        Anzahl der Subplot-Reihen.
+    cols : int, optional
+        Anzahl der Subplot-Spalten.
+
+    Returns:
+    -----------
+    None
+    """
+    for current_data, group_index in prepare_data(data, x_key, y_key, yerr_name, rows, cols):
+        fig, axes = plt.subplots(rows, cols, figsize=(8, 12), sharex=True, sharey=shared_y)
+        fig.subplots_adjust(hspace=0, wspace=0)
+
+        for i, (line_name, line_data) in enumerate(current_data):
+            row, col = divmod(i, cols)
+            ax = axes[row, col]
+
+            x_values = np.array(line_data.get(x_key, []))
+            y_values = np.array(line_data.get(y_key, []))
+            yerr_values = np.array(line_data.get(yerr_name, [])) if yerr_name else None
+            color = color_dict.get(line_name, 'black') if color_dict else 'black'
+
+            exponent_value = -15
+            exponent = 10 ** exponent_value
+            latex_exponent = f"10^{{{exponent_value}}}"
+
+            ylabel_parts = ylabel.split("[")
+            new_ylabel = ylabel_parts[0] + f"[{latex_exponent} " + ylabel_parts[1]
+
+            y_values = y_values / exponent
+            yerr_values = yerr_values / exponent
+
+            configure_lightcurves_axis(ax, row, col, new_ylabel, color, x_values, y_values, yerr_values, line_name)
+
+        finalize_figure(fig, axes, x_label=xlabel, title=title, group_index=group_index,
+                        save_only=save_only, output_dir=output_dir, compare_cont=compare_cont)
+
+
+def configure_lightcurves_axis(ax, row, col, ylabel, color, x_values, y_values, yerr_values, line_name):
+    """
+    Konfiguriert die Achse für Lightcurves.
+
+    Parameter:
+    -----------
+    ax : matplotlib.axes.Axes
+        Die jeweilige Achse, auf der geplottet wird.
+    row : int
+        Zeilenindex des Subplots.
+    col : int
+        Spaltenindex des Subplots.
+    ylabel : str
+        Beschriftung der Y-Achse.
+    color : str
+        Linienfarbe.
+    x_values : np.ndarray
+        X-Daten für den Plot.
+    y_values : np.ndarray
+        Y-Daten für den Plot.
+    yerr_values : np.ndarray or None
+        Fehlerbalkendaten, falls vorhanden.
+    line_name : str
+        Name / Label für die Datenlinie.
+
+    Returns:
+    -----------
+    None
+    """
+    if x_values.size > 0 and y_values.size > 0:
+        if yerr_values is not None:
+            ax.errorbar(
+                format_relative_days(x_values),
+                y_values,
+                yerr=yerr_values,
+                fmt='.:', capsize=3, markersize=4, label=f'{line_name}', color=color
+            )
+        else:
+            ax.plot(
+                format_relative_days(x_values),
+                y_values, label=f'{line_name}', color=color
+            )
+
+        ax.legend(fontsize=8, loc='upper right')
+
+    if col == 0:
+        ax.set_ylabel(ylabel, fontsize=12)
+        ax.yaxis.set_label_coords(-0.19, 0.5)
+    else:
+        ax.yaxis.tick_right()
+        ax.yaxis.set_label_position("right")
+        ax.set_ylabel(ylabel, fontsize=12)
+        ax.yaxis.set_label_coords(1.19, 0.5)
+
+    if row < 3:
+        ax.set_xticklabels([])
+
+    ax.xaxis.set_major_locator(MultipleLocator(5))
+    ax.yaxis.set_major_locator(MaxNLocator(nbins=5))
+    ax.yaxis.set_major_formatter(FuncFormatter(format_yaxis))
+
+    if row == 0:
+        ax_top = ax.secondary_xaxis('top')
+        ax_top.xaxis.set_major_locator(MultipleLocator(5))
+        ax_top.xaxis.set_major_formatter(FuncFormatter(format_month_day))
+        ax_top.tick_params(axis='x', rotation=45, labelsize=10)
--- run_tasks.py
+++ run_tasks.py
@@ -12,10 +12,10 @@ from handle_data.dopplershift import calc_broad_lines_doppler_shift_with_error
 from import_data.import_data import import_1d_correlation_data, load_dopplershift_data_from_toml, \
     import_1d_lightcurve_data, import_fits_data
 from plot_data.plot_1D_correlation_data import process_1d_correlations, compare_plots_across_continua
-from plot_data.plot_1D_ccfs_data import plot_all_1d_ccfs_in_groups_for_cont
+from plot_data.plot_1D_ccfs_in_groups_data import plot_all_1d_ccfs_in_groups_for_cont
 from plot_data.plot_time_lag import plot_fit_results, plot_time_lags_from_toml
-from plot_data.plot_1d_lightcurves_data import plot_1d_lightcurves, plot_1d_lightcurves_with_offset, \
-    plot_all_1d_lightcurves_in_groups
+from plot_data.plot_1d_lightcurves_data import plot_1d_lightcurves, plot_1d_lightcurves_with_offset
+from plot_data.plot_1D_lightcurves_in_groups_data import plot_all_1d_lightcurves_in_groups
 from plot_data.plot_fits_data import plot_avg_rms
 from settings import DEFAULT_OUTPUT_DIR
 
